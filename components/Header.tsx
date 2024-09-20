6a52ac1c6ebb4f27eb1164950907065966e2384d
Mladen Antic <mladen.antic98@gmail.com>
2024-09-20 12:47:24 +0200

    Adding landing page

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import {
   OrganizationSwitcher,
+  SignedIn,
   SignedOut,
   SignInButton,
   UserButton,
@@ -10,16 +11,18 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <div className="border-b py-4 bg-gray-50">
+    <div className="border-b py-4 bg-gray-50 relative z-10">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/" className="flex gap-2 items-center text-xl">
           <Image src="/logo.png" width={50} height={50} alt="FileDrive Logo" />
           FileDrive
         </Link>
 
-        <Button variant="outline">
-          <Link href="/dashboard/files">Your Files</Link>
-        </Button>
+        <SignedIn>
+          <Button variant="outline">
+            <Link href="/dashboard/files">Your Files</Link>
+          </Button>
+        </SignedIn>
 
         <div className="flex gap-2">
           <OrganizationSwitcher />
